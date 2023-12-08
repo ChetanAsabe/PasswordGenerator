a35f03ff4275568395134d3336ab81c6b565a220
Chetan Asabe <chetan27as@gmail.com>
2023-12-08 18:27:37 +0530

    user guide added

--- src/Home.jsx
+++ src/Home.jsx
@@ -18,13 +18,14 @@ const Home = () => {
     const val = document.getElementById("input");
     val.value = password;
     val.select();
-    document.execCommand('copy')
-  }
+    document.execCommand("copy");
+  };
 
   return (
-    <div className="bg-slate-50 shadow-2xl hover:shadow-2xl hover:shadow-blue-500/20 w-96 h-64 rounded-xl">
+    <div className="bg-slate-50 shadow-2xl hover:shadow-2xl hover:shadow-blue-500/20 w-96 h-72 rounded-xl">
       <div className="text-2xl text-black font-bold py-8">
         <h2>Password Generator</h2>
+        
       </div>
 
       <div className="pb-8">
@@ -34,6 +35,9 @@ const Home = () => {
           value={password}
           className="text-black border border-slate-400  bg-white w-2/3 rounded-lg px-3 py-2 "
         />
+        <p className="text-xs text-start pl-[4.2rem] text-slate-500 pt-1 font-normal opacity-50">
+          simply click on Generate button.
+        </p>
       </div>
 
       <div className="flex justify-evenly">
